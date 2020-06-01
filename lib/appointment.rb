c7a4ed7075e115db6b1ec09578ee96a22f31b8b9
Ian Hutson <ih00209@georgiasouthern.edu>
2020-06-01 23:21:49 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,3 +1,15 @@
 class Appointment
+  attr_accessor :name, :patient, :doctor
+  @@all = []
   
+  def initialize(name, artist, genre)
+    @name = name
+    @genre = genre
+    @artist = artist
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
